b4beb7a9baad3f97af31f643ac2f95994f20c9a2
Yetty2020 <fatihaadetoro@gmail.com>
2026-05-11 17:14:49 +0100

    feat: defined navigation link contants and types

---
+++ src/components/constants/Navigation.ts
@@ -0,0 +1,33 @@
+export interface Navlink {
+    title: string;
+    link: string;
+}
+
+export const navlinks: Navlink[] = [
+    {
+        title: "About",
+        link: "/about"
+    },
+    {
+        title: "Services",
+        link: "/services"
+    },
+    {
+        title: "For lawyers",
+        link: "/for-lawyers"
+
+    },
+    { 
+
+        title: "Results",
+        link: "/results"
+    },
+    {
+        title: "Blogs",
+        link: "/blogs"
+    },
+    {
+        title: "Get Started",
+        link: "/get-started"
+    }
+]
