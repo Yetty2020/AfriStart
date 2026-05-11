30c4faea4156a8da3ed7d2e0fa9475b29cc6656e
Yetty2020 <fatihaadetoro@gmail.com>
2026-05-11 17:06:39 +0100

    chore: Installed react icons

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -1,4 +1,4 @@
-
+import { IoDiamondOutline } from "react-icons/io5";
 
 function Hero() {
   return (
@@ -6,6 +6,7 @@ function Hero() {
         <span>Our Story</span>
         <h1>Built by women</h1>
         <h1>Built for founders</h1>
+        <span><IoDiamondOutline/></span>
         <p>
             We started Afristart Soluitons because we saw too many brilliant Nigerian entrepreneurs lose opportunities that they deserved - not because of bad business but because of bad structure.
         </p>
