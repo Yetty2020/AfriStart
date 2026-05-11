c8076662f5b68c240838111dfecf1bd2be717a78
Yetty2020 <fatihaadetoro@gmail.com>
2026-05-11 18:24:31 +0100

    feat: Implement toggle state management for mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,47 @@
-
+import { useState } from "react";
+import { navlinks } from "./constants/Navigation";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { RiCloseFill } from "react-icons/ri";
 
 function Navbar() {
+
+    const [isopenMenu, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen((prev) => !prev)
+        console.log("open")
+    }
   return (
-    <div>
+    <nav className="w-full flex justify-between ">
+         <div>
+                <span>Logo</span>
+            </div>
       
-    </div>
+
+           
+            <ul className="hidden lg:flex justify-between items-center gap-10">
+                {
+                navlinks.map((link, index) => {
+                    return (
+                        <li key={index} className="list-none">
+                            <a>
+                            {link.title}</a></li>
+                    )
+
+                })
+
+            }
+
+            </ul>
+            
+       
+        <div className="flex  lg:hidden">
+           
+            <button onClick={toggleMenu}>{
+                isopenMenu ? <RiCloseFill /> : <HiMenuAlt3 />
+            }</button>
+        </div>
+    </nav>
   );
 }
 
