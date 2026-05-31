c1c1bd609b1cc26364d0cd3f482c6cbc4c7fb98a
Yetty2020 <fatihaadetoro@gmail.com>
2026-05-31 04:44:12 +0100

    feat: Imported fonts for the hero section

---
+++ src/section/Values.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Values() {
+  return (
+    <div>
+        <p>What we stand for</p>
+        <h3>Our Values</h3>
+      
+    </div>
+  )
+}
+
+export default Values
